15cc7d5e1ae8e6423e91737e135fbaed01059d80
baltonCome <baltoncome@gmail.com>
2021-11-18 00:10:18 +0200

    Tasks app

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: 'Appointment',
+        day: 'July 16th at 12:07am',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Appointment in the bar',
+        day: 'July 16th at 12:07am',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Football',
+        day: 'July 16th at 12:07am',
+        reminder: false
+    }
+];
